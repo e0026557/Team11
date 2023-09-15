1a39a74cb05c257dcf60e551de810ca3e0c2b16c
Wesley Cheah <65939868+e0026557@users.noreply.github.com>
2023-09-15 08:47:22 +0000

    Added footer and layout route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Toastify from "./shared/toast/Toastify";
 import Home from "./modules/home/Home";
 import Login from "./modules/user/Login";
-import Navbar from './shared/navigation/Navbar';
+import Layout from './shared/layout/Layout';
 
 const App = () => {
   return (
     <>
       {/* React Toastify */}
-      <Toastify/>
+      <Toastify />
       <Router>
-        <Navbar />
         <Routes>
-          <Route index element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route element={<Layout/>}>
+            <Route index element={<Home />} />
+            <Route path='/login' element={<Login />} />
+          </Route>
         </Routes>
       </Router>
     </>
