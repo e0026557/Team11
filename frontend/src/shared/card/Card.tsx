71cc87f0cf22f72e9a6112f30652aa1217cd3671
Wesley Cheah <65939868+e0026557@users.noreply.github.com>
2023-10-17 01:12:32 +0000

    Added shared modal component and added modal for campsite reviews

--- frontend/src/shared/card/Card.tsx
+++ frontend/src/shared/card/Card.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../modal/Modal";
 
 export interface ICardProps {
   id: number;
@@ -9,7 +10,9 @@ export interface ICardProps {
   href: string;
 }
 
-const Card = (props: {card: ICardProps}) => {
+const Card = (props: { card: ICardProps }) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <div key={props.card.id}>
@@ -25,7 +28,10 @@ const Card = (props: {card: ICardProps}) => {
             <h3 className="text-sm font-medium text-gray-900">
               {props.card.name}
             </h3>
-            <p className="mt-1 text-sm text-gray-500">{props.card.detail}</p>
+            <p className="mt-1 text-sm text-gray-500">
+              <strong>Area: </strong>
+              {props.card.detail}
+            </p>
           </div>
           <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
             <div
@@ -35,14 +41,16 @@ const Card = (props: {card: ICardProps}) => {
           </div>
         </div>
         <div className="mt-6">
-          <a
-            href={props.card.href}
-            className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
+          <button
+            type="button"
+            className="w-full rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
+            onClick={() => setOpen(true)}
           >
-            Reserve<span className="sr-only">, {props.card.name}</span>
-          </a>
+            Reviews<span className="sr-only"> {props.card.name}</span>
+          </button>
         </div>
       </div>
+      <Modal open={open} setOpen={setOpen} />
     </>
   );
 };
