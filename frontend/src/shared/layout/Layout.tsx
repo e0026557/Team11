fcacd7fc426e04550b06c9a46bf88d3b8c9664f1
e0026557 <e0026557@u.nus.edu>
2023-10-21 13:48:34 +0800

    Fixed redirect to login bug

--- frontend/src/shared/layout/Layout.tsx
+++ frontend/src/shared/layout/Layout.tsx
@@ -1,14 +1,22 @@
 import React, { useEffect } from "react";
 import Navbar from "../navigation/Navbar";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Footer from "../footer/Footer";
 
 const Layout = () => {
   const navigate = useNavigate();
-  
+  const pathname = useLocation().pathname;
+
   useEffect(() => {
-    if (sessionStorage.getItem('userId') === null || sessionStorage.getItem('userId') === undefined) {
-      navigate('/login');
+    if (
+      sessionStorage.getItem("userId") === null ||
+      sessionStorage.getItem("userId") === undefined
+    ) {
+      console.log("pathname: ", pathname);
+      if (pathname === "/login" || pathname === "/register") {
+        return;
+      }
+      navigate("/login");
     }
   }, []);
 
@@ -16,7 +24,7 @@ const Layout = () => {
     <>
       <div className="min-h-screen bg-gray-150">
         <Navbar />
-          <Outlet />
+        <Outlet />
         <Footer />
       </div>
     </>
